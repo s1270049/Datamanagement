f482ff66d465d5251d143e83d403c2327ce125ed
s1270049 <s1270049@u-aizu.ac.jp>
2020-08-04 21:37:27 +0900

    HoT:Initial revision

---
+++ CoinToss.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+char CoinToss(int);
+#define N 3
+
+int main()
+{
+    char coin;
+    int i, j, num;
+    int loscou = 0, wincou = 0;
+    srand(time(NULL));
+    printf("Tossing a coin...\n");
+
+    for (i = 0; i < N; i++)
+    {
+        num = rand();
+        coin = CoinToss(num % 2);
+        printf("Round %d", i + 1);
+        if (coin == 'H')
+        {
+            wincou++;
+            printf("Heads\n");
+        }
+        else if (coin == 'T')
+        {
+            loscou++;
+            printf("Tails\n");
+        }
+    }
+    printf("Heads: %d, Tails: %d\n", wincou, loscou);
+    return 0;
+}
+char CoinToss(int num)
+{
+    if (num == 0)
+        return 'T';
+    else
+        return 'H';
+}
